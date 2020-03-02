c1070255c1b1ac8347a3039bad533bf602aa800f
Marco Salcedo <marcosalcedo@Marcos-MacBook-Pro.local>
2020-03-02 15:05:48 -0800

    adds post component for post detail

---
+++ wayfarer-app/src/components/PostDetail/PostDetailHeader/PostDetailHeader.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PostDetailHeader = props => {
+  return(
+    <>
+      <h1>This is a Post</h1>
+      <small>by Sarah Kerrigan</small>
+    </>
+  );
+};
+
+export default PostDetailHeader;
--- wayfarer-app/src/config/routes.js
+++ wayfarer-app/src/config/routes.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Home from '../components/Home/Home';
 import ProfileContainer from '../containers/ProfileContainer/ProfileContainer';
+import PostDetail from '../components/PostDetail/PostDetail';
 
 const Routes = props => {
   return (
     <Switch>
       <Route exact path='/' component={Home} />
       <Route path='/profile' component={ProfileContainer} />
+      <Route path='/post/detail' component={PostDetail} />
     </Switch>
   )
 }
