5f6ac72bd298d03cd59a4a500e1f51c785b4443f
Marco Salcedo <marcosalcedo@Marcos-MacBook-Pro.local>
2020-03-03 11:37:19 -0800

    modifies myposts style

--- wayfarer-app/src/components/Profile/MyPosts/MyPosts.js
+++ wayfarer-app/src/components/Profile/MyPosts/MyPosts.js
@@ -4,14 +4,11 @@ import PostMini from '../../PostList/PostMini/PostMini';
 
 const MyPosts = props => {
   return(
-    <div className="d-flex flex-column align-items-center justify-content-center m-5">
-      <h2>My Posts</h2>
       <div className="d-flex flex-column align-items-center justify-content-around">
         <PostMini />
         <PostMini />
         <PostMini />
       </div>
-    </div>
   );
 };
 
