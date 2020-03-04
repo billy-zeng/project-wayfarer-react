0f1fc2bef1d6eb47608a99b187e954563dfdfe3a
Marco Salcedo <marcosalcedo@Marcos-MacBook-Pro.local>
2020-03-04 14:19:52 -0800

    minor changes to citynav and cities.js

--- wayfarer-app/src/components/Cities/Cities.js
+++ wayfarer-app/src/components/Cities/Cities.js
@@ -6,7 +6,7 @@ import City from './City/City';
 const Cities = (props) => {
   return(
     <div className="container d-flex flex-row justify-content-center align-items-center">
-      <CityNav className="col-md-2 d-flex flex-row justify-content-center align-items-center"/>
+      <CityNav />
       <City className="col-md-10 d-flex flex-column justify-content-center align-items-center"/>
     </div>
   );
