3ad6f8f16503163f7e3c87b46c6fef30b7fddcef
Marco Salcedo <marcosalcedo@Marcos-MacBook-Pro.local>
2020-03-05 10:43:56 -0800

    fixes postmini style

--- wayfarer-app/src/components/PostList/PostInteraction/PostInteraction.js
+++ wayfarer-app/src/components/PostList/PostInteraction/PostInteraction.js
@@ -3,8 +3,8 @@ import React from 'react';
 const PostInteraction = props => {
   return (
     <div className="container d-flex flex-row align-items-end justify-content-end">
-      <a className="btn btn-info">Edit Post</a>
-      <a className="btn btn-danger">Delete Post</a>
+      <a className="btn btn-primary text-light mr-3">Edit Post</a>
+      <a className="btn btn-danger text-light mr-3">Delete Post</a>
     </div>
   );
 };
