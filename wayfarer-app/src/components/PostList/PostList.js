a992491c95bfe277dc3d51e3c2956a78ca50560e
billy-zeng <bzeng95@gmail.com>
2020-03-05 10:01:34 -0800

    fixes some styling

--- wayfarer-app/src/components/PostList/PostList.js
+++ wayfarer-app/src/components/PostList/PostList.js
@@ -5,6 +5,8 @@ import PostListHeader from './PostListHeader/PostListHeader';
 import PostMini from './PostMini/PostMini';
 
 class PostList extends React.Component {
+
+
   displayPosts = posts => {
     return posts.map(post => {
       return <PostMini key={post._id} postData={post} />
@@ -12,7 +14,6 @@ class PostList extends React.Component {
   };
 
   render() {
-    // console.log(this.props.posts)
     return (
       <div className="container d-flex flex-column justify-content-center align-items-center">
         <PostListHeader />
--- wayfarer-app/src/components/PostList/PostMini/PostMini.js
+++ wayfarer-app/src/components/PostList/PostMini/PostMini.js
@@ -10,14 +10,14 @@ const PostMini = props => {
       <div className="container col-sm-3 d-flex flex-column align-items-center justify-content-center">
         <img className="rounded border post-mini-img" src="https://images.unsplash.com/photo-1570432871838-d61aed09f144?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"/>
       </div>
-      <div className="col-sm-9 d-flex flex-column align-items-center jutify-content-center">
-        <div className="bg-dark m-2 p-2 rounded container row-sm-2 d-flex flex-column align-items-center jutify-content-center">
+      <div className="col-sm-9 d-flex flex-column align-items-center justify-content-center">
+        <div className="bg-dark m-2 p-2 rounded container row-sm-2 d-flex flex-column align-items-center justify-content-center">
           <Link to="/post/detail" className="d-flex align-self-start post-mini-title ">
             <h4 className="d-flex align-self-start post-mini-title text-light">{props.postData.title}</h4>
           </Link>
           <small className="d-flex align-self-start text-light">by {props.postData.author}</small>
         </div>
-        <div className="row-sm-2 d-flex flex-column align-items-center jutify-content-center">
+        <div className="row-sm-2 d-flex flex-column align-items-center justify-content-center">
           <p>{props.postData.content}</p>
         </div>
       </div>
